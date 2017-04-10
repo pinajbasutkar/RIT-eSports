74a6c392f963dee059aac32ce4b792e61a6532aa
bjm4054 <bjm4054@rit.edu>
2017-04-10 16:48:24 -0400

    Added SQLite interaction to get list of teams.  Still need to dynamically create screen elements.
    Added SQLite interaction to get list of teams.  Still need to
    dynamically create screen elements.
    Replaced hard-coded team reference with team.php.

--- teams.php
+++ teams.php
@@ -5,20 +5,49 @@
   <?php include 'includes/head.php';?>
 </head>
 <body>
-	
-    <?php include 'includes/navmenu.php';?>
-    
 	<main>
+		<?php
+			include 'includes/navmenu.php';
+			   
+			class ESportsDB extends SQLite3
+			{
+				function __construct()
+				{
+					$this->open('db/sqlite/sqlite-tools-win32-x86-3180000/ESports.db');
+				}
+			}
+			
+			$esports_db = new ESportsDB();
+			
+			if(!$esports_db)
+			{
+				echo $esports_db->lastErrorMsg();
+			}
+			else
+			{
+				echo "Database opened successfully\n\n";			
+			}
+			
+			$esports_db->exec("ATTACH DATABASE 'ESports.db' AS 'esports'");
+			$team_list_result = $esports_db->query("SELECT * FROM TEAMS");	
+			
+			while($team = $team_list_result->fetchArray(SQLITE3_ASSOC))
+			{
+				echo "<br>TEAM_ID: " . $team['TEAM_ID'];
+				echo "<br>TEAM_NAME: " . $team['TEAM_NAME'];
+				echo "<br>GAME: " . $team['GAME'] . "<br>";
+			}			
+        ?>	
 		<div class="container-fluid">
 			<h2 class="page_title">TEAMS</h2>
 			
 			<div class="row">
 				<div class="col-sm-4 col-md-4">
 					<figure>
-                        <a href="league_of_legends_one.php">
+                        <a href="team.php?team_id=1">
 							<img class="img-circle img-responsive center-all" src="media/esports_assets/tigerhead_small.png" alt="Link to TBD Team Page" title="Link to TBD Team Page">
 							<!--<figcaption id="single_team_name" class="figure-caption team_name">League of Legends One</figcaption> -->
-							<figcaption id="single_team_name" class="figure-caption text-center team_name">League of Legends One</figcaption>
+							<figcaption id="single_team_name" class="figure-caption text-center team_name">Counter Strike</figcaption>
 						</a>
 					</figure>
 				</div>
