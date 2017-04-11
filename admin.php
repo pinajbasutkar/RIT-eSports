5513705a41f59337d7c61114d7bdc165d02f8b5f
amf007 <andrewfagan@Andrews-MacBook-Pro.local>
2017-04-11 19:34:13 -0400

    update admin title/secondary nav
    Also removed random closing divs that did not belong to anything (??)

--- admin.php
+++ admin.php
@@ -9,45 +9,45 @@
     <?php include 'includes/navmenu.php';?>
     
 	<main>
+    
+        <h2 class="page_title">ADMIN FORMS</h2>
 
-<div class="tab_admin">
-  <button class="tablinks" onclick="change(event, 'AddNews')" id="defaultOpen">Add News</button>
-  <button class="tablinks" onclick="change(event, 'EditNews')">Edit News</button>
-  <button class="tablinks" onclick="change(event, 'AddTeam')">Add Team</button>
-  <button class="tablinks" onclick="change(event, 'EditTeam')">Edit Team</button>
-  <button class="tablinks" onclick="change(event, 'AddMatch')">Add Match</button>
-  <button class="tablinks" onclick="change(event, 'EditMatch')">Edit Match</button>
+        <div class="tab_admin">
+          <button class="tablinks" onclick="change(event, 'AddNews')" id="defaultOpen">Add News</button>
+          <button class="tablinks" onclick="change(event, 'EditNews')">Edit News</button>
+          <button class="tablinks" onclick="change(event, 'AddTeam')">Add Team</button>
+          <button class="tablinks" onclick="change(event, 'EditTeam')">Edit Team</button>
+          <button class="tablinks" onclick="change(event, 'AddMatch')">Add Match</button>
+          <button class="tablinks" onclick="change(event, 'EditMatch')">Edit Match</button>
 
-</div>
+        </div>
 
-<br>
+        <br>
 
-<div id="AddNews" class="tabcontent">
-		    <h2 class="page_title">ADD NEWS</h2>
-			<form>	  
-				<div class="form-group">
-					<label for="news_heading">News Heading</label>
-					<text class="form-control" rows="1" id="newsheading"></text>
-				</div>
-				
-				<div class="form-group">
-					<label for="published_on">Published On</label>
-					<text class="form-control" rows="1" id="published_on"></text>
-				</div>
+        <div id="AddNews" class="tabcontent">
+            <form>	  
+                <div class="form-group">
+                    <label for="news_heading">News Heading</label>
+                    <text class="form-control" rows="1" id="newsheading"></text>
+                </div>
 
-				<div class="form-group">
-					<label for="description">Description</label>
-					<textarea class="form-control" rows="8" id="description"></textarea>
-				</div>
-			  
-				<button type="button" class="btn btn-warning main_action_button">Publish</button>
-			</div>			
-			</form>
-</div>   
+                <div class="form-group">
+                    <label for="published_on">Published On</label>
+                    <text class="form-control" rows="1" id="published_on"></text>
+                </div>
+
+                <div class="form-group">
+                    <label for="description">Description</label>
+                    <textarea class="form-control" rows="8" id="description"></textarea>
+                </div>
+
+                <button type="button" class="btn btn-warning main_action_button">Publish</button>
+
+            </form>
+        </div>   
     
 
 		<div id="EditNews" class="tabcontent">
-		    <h2 class="page_title">EDIT NEWS</h2>
 			<form>	  
 				<div class="form-group">
 					<label for="news_heading">News Heading</label>
@@ -64,14 +64,12 @@
 					<textarea class="form-control" rows="8" id="description"></textarea>
 				</div>
 			  
-				<button type="button" class="btn btn-warning main_action_button">Publish</button>
-			</div>			
+				<button type="button" class="btn btn-warning main_action_button">Publish</button>		
 			</form>
 		</div>
         
 		
         <div id="AddTeam" class="tabcontent">
-		    <h2 class="page_title">ADD TEAM</h2>
 			<form>	  
 				<div class="form-group">
 					<label for="team_name">Team Name</label>
@@ -93,14 +91,12 @@
 					<text class="form-control" rows="1" id="player_image"></text>
 				</div>
 			  
-				<button type="button" class="btn btn-warning main_action_button">Publish</button>
-			</div>			
+				<button type="button" class="btn btn-warning main_action_button">Publish</button>	
 			</form>
-    </div>		
+        </div>		
     
 			
         <div id="EditTeam" class="tabcontent">
-		    <h2 class="page_title">EDIT TEAM</h2>
 			<form>	  
 				<div class="form-group">
 					<label for="team_name">Team Name</label>
@@ -122,15 +118,11 @@
 					<text class="form-control" rows="1" id="player_name"></text>
 				</div>
 				
-				<button type="button" class="btn btn-warning main_action_button">Publish</button>
-			</div>			
+				<button type="button" class="btn btn-warning main_action_button">Publish</button>	
 			</form>
-    </div>	
-    
-    
+        </div>	
 
         <div id="AddMatch" class="tabcontent">
-		    <h2 class="page_title">ADD MATCH</h2>
 			<form>	  
 				<div class="form-group">
 					<label for="team1">Team 1</label>
@@ -157,45 +149,40 @@
 					<text class="form-control" rows="1" id="video"></text>
 				</div>
 			  
-				<button type="button" class="btn btn-warning main_action_button">Publish</button>
-			</div>			
+				<button type="button" class="btn btn-warning main_action_button">Publish</button>		
 			</form>
-    </div>		
+        </div>		
     
-    
-		
-		
-     <div id="EditMatch" class="tabcontent">
-		    <h2 class="page_title">EDIT MATCH</h2>
-			<form>	  
-				<div class="form-group">
-					<label for="team1">Team 1</label>
-					<text class="form-control" rows="1" id="team1"></text>
-				</div>
-				
-				<div class="form-group">
-					<label for="team2">Team 2</label>
-					<text class="form-control" rows="1" id="team2"></text>
-				</div>
 
-				<div class="form-group">
-					<label for="score">Score</label>
-					<text class="form-control" rows="1" id="score"></text>
-				</div>
-				
-				<div class="form-group">
-					<label for="time">Time</label>
-					<text class="form-control" rows="1" id="time"></text>
-				</div>
-				
-				<div class="form-group">
-					<label for="video">Video Link</label>
-					<text class="form-control" rows="1" id="video"></text>
-				</div>
-				<button type="button" class="btn btn-warning main_action_button">Publish</button>
-			</div>			
-			</form>
-    </div>	
+        <div id="EditMatch" class="tabcontent">
+            <form>	  
+                <div class="form-group">
+                    <label for="team1">Team 1</label>
+                    <text class="form-control" rows="1" id="team1"></text>
+                </div>
+
+                <div class="form-group">
+                    <label for="team2">Team 2</label>
+                    <text class="form-control" rows="1" id="team2"></text>
+                </div>
+
+                <div class="form-group">
+                    <label for="score">Score</label>
+                    <text class="form-control" rows="1" id="score"></text>
+                </div>
+
+                <div class="form-group">
+                    <label for="time">Time</label>
+                    <text class="form-control" rows="1" id="time"></text>
+                </div>
+
+                <div class="form-group">
+                    <label for="video">Video Link</label>
+                    <text class="form-control" rows="1" id="video"></text>
+                </div>
+                <button type="button" class="btn btn-warning main_action_button">Publish</button>		
+            </form>
+        </div>	
 </main>
 
 <br><br><br>
