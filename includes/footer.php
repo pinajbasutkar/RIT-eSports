d7b7971b0b7aa213d30bc232c4a6ca5241e0d0aa
amf007 <andrewfagan@Andrews-MacBook-Pro.local>
2017-04-11 19:27:53 -0400

    updated footer
    Darker admin login button
    larger social media icons
    centered social media icons and copyright

--- includes/footer.php
+++ includes/footer.php
@@ -1,17 +1,17 @@
     <footer>
     
-        <div class="row">
+        <div class="row center-all">
         
-            <a href="http://www.facebook.com/esportsrit" target="_blank"><i class="fa fa-facebook-square fa-2x"></i></a>
-            <a href="http://www.instagram.com/ritesports" target="_blank"><i class="fa fa-instagram fa-2x"></i></a>
+            <a href="http://www.facebook.com/esportsrit" target="_blank"><i class="fa fa-facebook-square fa-3x"></i></a>
+            <a href="http://www.instagram.com/ritesports" target="_blank"><i class="fa fa-instagram fa-3x"></i></a>
             <a href="https://discord.gg/XvcyYgQ" target="_blank"><img class="discord-logo" src="media/discord_logo.png" alt="Discord logo" /></a>
-            <a href="http://www.twitter.com/ritesports" target="_blank"><i class="fa fa-twitter-square fa-2x"></i></a>
-            <a href="http://www.twitch.tv/ritesports" target="_blank"><i class="fa fa-twitch fa-2x"></i></a>
+            <a href="http://www.twitter.com/ritesports" target="_blank"><i class="fa fa-twitter-square fa-3x"></i></a>
+            <a href="http://www.twitch.tv/ritesports" target="_blank"><i class="fa fa-twitch fa-3x"></i></a>
             
         </div>
             
             
-        <div class="row">
+        <div class="row text-center">
         
             <p id="copyright">
 			    &copy; <?php echo date("Y"); ?> RITeSports 
