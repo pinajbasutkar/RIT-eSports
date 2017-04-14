494e0cc8dd98f8a9290993915cefd3b68089b90c
bjm4054 <bjm4054@rit.edu>
2017-04-13 22:39:05 -0400

    Implemented dynamic population of news items
    We're pulling from the database now

--- index.php
+++ index.php
@@ -5,6 +5,26 @@
   <?php include 'includes/head.php';?>
 </head>
 <body>
+	
+    <div id="main_home_image">
+		<img class="img-responsive center-all" id="bg-gif" src="media/home-bg.gif"/>
+		<img class="img-responsive center-all" id="bg-overlay" src="media/home-bg-overlay.png"/>
+    </div>
+	
+    <main class="center-div">
+        
+        <h2 class="page_title">ABOUT</h2>	
+        <p>
+           RIT eSports is a student-run organization within the Rochester Institute of Technology, with the goal of facilitating the development and growth of gaming talent on campus. 
+		   We are one of the fastest growing organizations at RIT, with over 70 players currently active in games such as League of Legends, Overwatch, Heroes of the Storm, 
+		   Counter Strike, Starcraft, Rocket League, and Dota, with even more on the way soon. Win or lose, we are constantly striving for self-improvement, 
+		   be it as players, students, or local citizens. We pride ourselves on teamwork, leadership, and determination, modeling ourselves after both professional eSport teams 
+		   and traditional collegiate athletics. Due to our traditional inspiration and unique campus composition, we believe that in the next few years, RIT can become the #1 college 
+		   in the world for competitive gaming.
+        </p>  
+            
+	</main>
+
 	<?php
 	    include 'includes/navmenu.php';
 	    
@@ -22,98 +42,36 @@
         {
 		    echo $esports_db->lastErrorMsg();
 		}
-	?>
+
+		$esports_db->exec("ATTACH DATABASE 'ESports.db' AS 'esports'");		
+		$news_item_list = $esports_db->query("SELECT * FROM NEWS_ITEMS");
+			
+		echo "<section id='news_and_events' class='row container'>";
+		echo "<div class='container center-all'>";
+		echo "<h2 class='page_title'>NEWS AND EVENTS</h2>";          
+
+				while($news_item = $news_item_list->fetchArray(SQLITE3_ASSOC))
+				{
+					$image_url = $news_item['IMAGE_URL'];
+					echo '<div class="col-md-6 col-lg-6 container">';			
+					echo '<div class="news-item">';
+					echo "<a href='$image_url'>";
+					echo "<img src='$image_url' alt='News Item Picture' title='News Item Picture' class='img-responsive center-div'>";
+					$headline = $news_item['HEADLINE'];
+					echo "<h3 class='news-item-title'>$headline</h3></a>";
+					$author = $news_item['AUTHOR'];
+					$date = $news_item['DATE'];
+					echo "<p class='news-item-info'>by $author | $date</p>";
+					echo '<p class="news-item-text">';
+					$intro = $news_item['INTRO'];
+					echo "$intro</p></div></div>";
+				}
+			
+		echo '<div class="center-all">';
+		echo '<a href="[TO BE DETERMINED]" class="center-all">';
+		echo '<button type="button" class="btn btn-warning btn-lg" id="load_more_button">Load More</button></a></div></div></section>';
+    ?>
 	
-    <div id="main_home_image">
-    <img class="img-responsive center-all" id="bg-gif" src="media/home-bg.gif"/>
-    <img class="img-responsive center-all" id="bg-overlay" src="media/home-bg-overlay.png"/>
-</div>
-	
-    <main class="center-div">
-        
-        <h2 class="page_title">ABOUT</h2>	
-        <p>
-           RIT eSports is a student run organization within the Rochester Institute of Technology, with the goal of facilitating the development and growth of gaming talent on campus. 
-		   We are one of the fastest growing organizations at RIT, with over 70 players currently active. Teams include games such as League of Legends, Overwatch, Heroes of the Storm, 
-		   Counter Strike, Starcraft, Rocket League, and Dota, with even more on the way soon. Whether a team wins or loses, we are constantly striving for self-improvement, 
-		   whether it be as players, students, or local citizens. We pride ourselves on teamwork, leadership, and determination, modeling ourselves after both professional eSport teams 
-		   and traditional collegiate athletics programs. Due to our traditional inspiration, as well as our unique campus composition, we believe that RIT can become the #1 college 
-		   in the world for competitive gaming within the next few years.
-        </p>  
-            
-	</main>
-		
-	<section id="news_and_events"  class="row container">
-		<div class="container center-all">
-			<h2 class="page_title">NEWS AND EVENTS</h2>
-            
-            
-            
-            <div class="col-md-6 col-lg-6 container">
-				<div class="news-item">
-					<a href="media/news_events/gaming_background.jpg">
-						<img src="media/news_events/gaming_background.jpg" alt="News Item Picture" title="News Item Picture" class="img-responsive center-div">	
-                        <h3 class="news-item-title">Welcome to RIT eSports</h3>
-					</a>
-                    <p class="news-item-info">by John Cena | Feb 27, 2015</p>
-                    <p class="news-item-text">
-                        Welcome to esportsrit.com! This is the official website for RIT eSports, one of the fastest growing collegiate eSports...
-                    </p>
-				</div>
-			</div>
-            
-			<div class="col-md-6 col-lg-6 container">
-				<div class="news-item">
-					<a href="media/news_events/gaming_background.jpg">
-						<img src="media/news_events/gaming_background.jpg" alt="News Item Picture" title="News Item Picture" class="img-responsive center-div">	
-                        <h3 class="news-item-title">Welcome to RIT eSports</h3>
-					</a>
-                    <p class="news-item-info">by John Cena | Feb 27, 2015</p>
-                    <p class="news-item-text">
-                        Welcome to esportsrit.com! This is the official website for RIT eSports, one of the fastest growing collegiate eSports...
-                    </p>
-				</div>
-			</div>
-            
-			<div class="col-md-6 col-lg-6 container">
-				<div class="news-item">
-					<a href="media/news_events/gaming_background.jpg">
-						<img src="media/news_events/gaming_background.jpg" alt="News Item Picture" title="News Item Picture" class="img-responsive center-div">	
-                        <h3 class="news-item-title">Welcome to RIT eSports</h3>
-					</a>
-                    <p class="news-item-info">by John Cena | Feb 27, 2015</p>
-                    <p class="news-item-text">
-                        Welcome to esportsrit.com! This is the official website for RIT eSports, one of the fastest growing collegiate eSports...
-                    </p>
-				</div>
-			</div>
-            
-			<div class="col-md-6 col-lg-6 container">
-				<div class="news-item">
-					<a href="media/news_events/gaming_background.jpg">
-						<img src="media/news_events/gaming_background.jpg" alt="News Item Picture" title="News Item Picture" class="img-responsive center-div">	
-                        <h3 class="news-item-title">Welcome to RIT eSports</h3>
-					</a>
-                    <p class="news-item-info">by John Cena | Feb 27, 2015</p>
-                    <p class="news-item-text">
-                        Welcome to esportsrit.com! This is the official website for RIT eSports, one of the fastest growing collegiate eSports...
-                    </p>
-				</div>
-			</div>
-
-		
-            <div class="center-all">
-
-                <a href="[TO BE DETERMINED]" class="center-all">
-                    <button type="button" class="btn btn-warning btn-lg" id="load_more_button">Load More</button>
-                </a> 
-
-            </div>
-
-        </div>
-		
-    </section> 
-
     <hr id="footer_line">	
 	
 	<?php
