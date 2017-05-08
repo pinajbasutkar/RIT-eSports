07e1ec70b9abcec612160d023f3f416fede40225
amf007 <andrewfagan@Andrews-MacBook-Pro.local>
2017-05-08 18:04:43 -0400

    admin migration and delete login page

---
+++ admin_pages/includes/footer.php
@@ -0,0 +1,40 @@
+    <footer>
+ 
+<!--Back to Top button starts----------------------------------------------------------------------->   
+ 
+    <a href="#" class="back-to-top" style="display: inline;">
+ 
+    <i class="fa fa-chevron-circle-up"></i>
+ 
+    </a>
+        
+<!--Back to Top button ends----------------------------------------------------------------------->    
+    
+        <div class="row center-all" id="social-icons">
+        
+            <a href="http://www.facebook.com/esportsrit" target="_blank"><i class="fa fa-facebook-square fa-3x"></i></a>
+            <a href="http://www.instagram.com/ritesports" target="_blank"><i class="fa fa-instagram fa-3x"></i></a>
+            <a href="https://discord.gg/XvcyYgQ" target="_blank"><img class="discord-logo" src="media/discord_logo.png" alt="Discord logo" /></a>
+            <a href="http://www.twitter.com/ritesports" target="_blank"><i class="fa fa-twitter-square fa-3x"></i></a>
+            <a href="http://www.twitch.tv/ritesports" target="_blank"><i class="fa fa-twitch fa-3x"></i></a>
+            
+        </div>
+            
+            
+        <div class="row text-center">
+        
+            <p id="copyright">
+			    &copy; <?php echo date("Y"); ?> RITeSports 
+			</p>
+        
+        </div> 
+        
+        <div class="row">
+        
+            <a href="admin_pages/admin_add_news.php">
+                <button type="button" class="btn btn-default" id="admin_login_button">Admin Login</button>
+            </a> 
+        
+        </div>
+        
+    </footer>
---
+++ admin_pages/includes/navmenu.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-left" href="index.php">
+			    <img id="header_logo" src="media/esports_assets/Logo1.png" alt="RIT eSports Logo" title="RIT eSports Logo">
+			</a>		
+		    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#esports_navbar">
+			    <span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+        </div>		
+		<div class="collapse navbar-collapse" id="esports_navbar">
+			<ul class="nav navbar-nav navbar-right">
+				<li>
+					<a href="index.php#news_and_events">NEWS</a>
+				</li>
+				<li class="active dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#">MATCHES<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="matches.php?type=past">Past</a></li>
+						<li><a href="matches.php?type=current">Current</a></li>
+						<li><a href="matches.php?type=future">Future</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="teams.php">TEAMS</a>	  
+				</li>
+				<li>
+					<a href="contact.php">CONTACT</a>
+				</li>
+			</ul>
+		</div>
+    </div>
+</nav>
